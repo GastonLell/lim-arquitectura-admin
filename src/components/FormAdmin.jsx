7efe64c4b5b42a52735c8eac93319bf319dafe2a
GastonLell <gastonglell@gmail.com>
2021-04-19 15:04:19 -0300

    form admin

--- src/components/FormAdmin.jsx
+++ src/components/FormAdmin.jsx
@@ -1,78 +1,141 @@
 //REACT
-import {useCallback} from "react";
+import { useState, useCallback } from "react";
 
 //FIREBASE
-import firebase from "firebase"
-import {getFirestore} from "../firebase/client";
+import firebase from "firebase";
+import { getFirestore } from "../firebase/client";
 
 //BOOTSTRAP
-import { Form, Button } from "react-bootstrap";
-
+import { Form, Row, Col, Button, Alert, ProgressBar } from "react-bootstrap";
 
 const FormAdmin = () => {
+  const [progress, setProgress] = useState({
+    mensaje: "",
+    porcentaje: null,
+  });
 
+  const handlePost = useCallback(async (event) => {
+    event.preventDefault();
 
-  const handlePost = useCallback(
+    const db = getFirestore();
 
-    async (event) => {
-      event.preventDefault();
-      
-      const db = getFirestore();
+    const { nombre, categoria, file } = event.target.elements;
 
-      const {nombre, categoria, file} = event.target.elements;
-      
-      try{
-        let imageDownloadUrl;
+    try {
+      let imageDownloadUrl;
 
-        const refStorage = await firebase.storage().ref(`proyectos/${file.files[0].name}`)
+      const refStorage = await firebase.storage().ref(`proyectos/${file.files[0].name}`);
 
-        if(!!file){
-      
-          await refStorage.put(file.files[0]).then(() => console.log("imagen en storage"))
-          .catch(err => console.log(err))
-      
-        }
+      if (!!file) {
+        const task = refStorage.put(file.files[0]);
 
-        await refStorage.getDownloadURL().then(url => {
-          imageDownloadUrl = url;
-        })
+        task.on("state_changed", (snapshot) => {
+            let porcentajeActual = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+            setProgress({
+              ...progress,
+              porcentaje: porcentajeActual,
+            });
+          }, (error) => {
+            setProgress({
+              ...progress,
+              mensaje: "error al cargar imagen. Por favor, intentelo nuevamente",
+            });
+          }, () => {
+            setProgress({
+              ...progress,
+              mensaje: "Imagen cargada",
+            })
+          });
+      }
 
-        await db.collection('proyectos').add({
+      await refStorage.getDownloadURL().then((url) => {
+        imageDownloadUrl = url;
+      });
+
+      await db
+        .collection("proyectos")
+        .add({
           nombre: nombre.value,
           categoria: categoria.value,
           srcImagen: imageDownloadUrl,
-          fecha: firebase.firestore.FieldValue.serverTimestamp()
+          fecha: firebase.firestore.FieldValue.serverTimestamp(),
         })
-        .then(refDoc => {
-          console.log(refDoc.id)
+        .then((refDoc) => {
+          console.log(refDoc.id);
         })
-        .catch(err => console.log(`error al enviar documentos a firebase ${err}`))
+        .catch((err) =>
+          console.log(`error al enviar documentos a firebase ${err}`)
+        );
       
-      } catch (error){
-        alert(`error al cargar imagen ${error}`)
-      }
-    
-    }, [])
+    } catch (error) {
+      alert(error)
+    }
 
+  }, [progress]);
 
+  const handleResetForm = () => {
+    document.getElementById("formAdmin").reset();
+    setProgress({
+      mensaje: "",
+      porcentaje: null,
+    })
+  }
   return (
-    <Form onSubmit={handlePost}>
-      <Form.Group>
-        <Form.Control name="nombre" type="text" placeholder="Ingrese el nombre..." className="my-4"/>
-      </Form.Group>
-      <Form.Group>
-        <Form.Control name="categoria" as="select" className="my-4">
-          <option selected>Seleccione categoria</option>
-          <option value="renders">Renders</option>
-          <option value="videos">Videos</option>
-          <option value="realidad">Realidad Virtual</option>
-        </Form.Control>
-      </Form.Group>
-      <Form.Group>
-        <Form.File name="file" className="my-4" />
-      </Form.Group>
-        <Button variant="outline-dark" type="submit" className="my-2 align-items-end">Guardar</Button>
-    </Form>
+    <>
+      <Col>
+      {
+        progress.porcentaje > 0 && (
+          <ProgressBar now={progress.porcentaje} striped variant="dark" label={`${progress.porcentaje}%`} />
+        )
+      }
+        <Form id="formAdmin" onSubmit={handlePost}>
+          <Form.Group>
+            <Form.Control
+              name="nombre"
+              type="text"
+              placeholder="Ingrese el nombre..."
+              className="my-4"
+              required
+            />
+          </Form.Group>
+          <Form.Group>
+            <Form.Control name="categoria" as="select" className="my-4" required>
+              <option selected>Seleccione categoria</option>
+              <option value="renders">Renders</option>
+              <option value="videos">Videos</option>
+              <option value="realidad">Realidad Virtual</option>
+            </Form.Control>
+          </Form.Group>
+          <Form.Group>
+            <Form.File name="file" className="my-4" required/>
+          </Form.Group>
+          <Row className="justify-content-end m-0">
+            <Button
+              variant="dark"
+              onClick={handleResetForm}
+              className="my-2 mx-2"
+            >
+              Reset
+            </Button>
+            <Button
+              variant="outline-dark"
+              type="submit"
+              className="my-2"
+            >
+              Guardar
+            </Button>
+          </Row>
+        </Form>
+        {
+          progress.mensaje && (
+            <Alert variant="dark">
+              {progress.mensaje}
+            </Alert>
+          )
+        }
+
+      </Col>
+    </>
   );
 };
 export default FormAdmin;
---
+++ src/pages/Mensajeria.jsx
@@ -0,0 +1,14 @@
+import {Container, Row, Col} from "react-bootstrap";
+const Mensajeria = () => {
+
+    return(
+        <Container>
+            <Row>
+                <Col>
+                
+                </Col>
+            </Row>
+        </Container>
+    )
+}
+export default Mensajeria;
